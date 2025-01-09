fe8024fde70892c80e645f99d8bc28b0aed4c893
MENTE-SAIRAM <122193266+MENTE-SAIRAM@users.noreply.github.com>
2025-01-09 14:17:13 +0530

    Update OAuth.jsx

--- frontend/src/Components/OAuth.jsx
+++ frontend/src/Components/OAuth.jsx
@@ -17,7 +17,7 @@ const OAuth = () => {
 
             const result = await signInWithPopup(auth, provider);
             
-            const res = await fetch("http://localhost:3000/api/auth/google", {
+            const res = await fetch("https://real-estate-application-2-x6ua.onrender.com/api/auth/google", {
                 method: "POST",
                 headers: {
                     "Content-Type": "application/json",
